0a51d4c9608a02966077f2d49a344cd3fc97dcbb
jesgogu27 <jesgogu@hotmail.com>
2020-02-05 17:17:38 -0500

    size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/* The code show a comment with push function*/
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*printf function*/
+printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+/*return value*/
+return (0);
+}
