d73872a8c0c4d7a42bceb94200166c7e43efacd9
jesgogu27 <jesgogu@hotmail.com>
2020-02-05 15:28:49 -0500

    print with push into c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S $CFILE > c
+/* The code show a comment with push function*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    /*variable*/
+    char g[60]="\"Programming is like building a multilingual puzzle";
+    /*puts function*/
+    puts (g);
+    return 0;
+}
