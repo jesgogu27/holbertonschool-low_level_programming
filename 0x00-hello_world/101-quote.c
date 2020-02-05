339035cddd7b6149eff984dd126e22d7f4a4cd99
jesgogu27 <jesgogu@hotmail.com>
2020-02-05 18:27:16 -0500

    different to puts and to printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-/*Message*/
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 /*Return*/
 return (1);
 }
