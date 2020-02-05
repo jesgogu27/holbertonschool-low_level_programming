20503f2b55032568f29e4ba9744b497a73e11533
jesgogu27 <jesgogu@hotmail.com>
2020-02-05 16:34:53 -0500

    print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/* The code show a comment with push function*/
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*printf function*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+/*return value*/
+return (0);
+}
