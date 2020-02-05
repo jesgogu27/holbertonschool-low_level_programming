3ee459730d78696e299a809d50b290466a03f70a
jesgogu27 <jesgogu@hotmail.com>
2020-02-05 17:35:18 -0500

    size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,20 +8,17 @@
  */
 int main(void)
 {
-
 char c;
 int i;
 long li;
 long long int lli;
 float f;
-
 /*printf function*/
-
-printf("Size of a char: %ld bytes(s)\n", sizeof(c));
-printf("Size of an int: %ld bytes(s)\n", sizeof(i));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(li));
-printf("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
-printf("Size of a float: %ld bytes(s)\n", sizeof(f));
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 /*return value*/
 return (0);
 }
