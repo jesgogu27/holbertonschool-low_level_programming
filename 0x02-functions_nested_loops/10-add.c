c0d08745a34aaa10aa6ff78a3537d1c81b8cc4d2
jesgogu27 <jesgogu@hotmail.com>
2020-02-11 18:38:29 -0500

    repush

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int add(int a, int b)
 {
-	int r;
 	
-	r = a + b;
-	return (r);
+int r;
+r = a + b;
+return (r);
 }
 
 
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-void jack_bauer(void){
+void jack_bauer(void)
+{
 
 	int a;
 	int b;
@@ -16,36 +17,36 @@ void jack_bauer(void){
 	c = 0;
 	d = 0;
 
-	while (a < 3)
-	{
-		while (b < 10)
-		{
-			if((a == 2) && (b == 4))
-			{
-				break;
-			}
-			else
-			{
-				while (c < 6)
-					{
-						while (d < 10)
-							{
-								_putchar('0'+a);
-								_putchar('0'+b);
-								_putchar('0'+10);
-								_putchar('0'+c);
-								_putchar('0'+d);
-								_putchar('\n');
-							d++;
-							}
-					d=0;
-					c++;
-					}
-			c=0;
-			b++;
-			}
-		}
-	b=0;
-	a++;
-	}
+while (a < 3)
+{
+while (b < 10)
+{
+if((a == 2) && (b == 4))
+{
+break;
+}
+else
+{
+while (c < 6)
+{
+while (d < 10)
+{
+_putchar('0'+a);
+_putchar('0'+b);
+_putchar('0'+10);
+_putchar('0'+c);
+_putchar('0'+d);
+_putchar('\n');
+d++;
+}
+d=0;
+c++;
+}
+c=0;
+b++;
+}
+}
+b=0;
+a++;
+}
 }
