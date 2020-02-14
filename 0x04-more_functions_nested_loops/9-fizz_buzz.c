51603abd5dd546266330239f3b26635b90ecf403
jesgogu27 <jesgogu@hotmail.com>
2020-02-13 23:28:36 -0500

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,10 @@ while (a <= 100)
 if ((a % 3 == 0) && (a % 5 == 0))
 {
 printf("FizzBuzz ");
-}else if (a % 5 == 0)
+}
+else
+if (a % 5 == 0)
+}
 {
 if (a == 100)
 {
@@ -21,7 +24,8 @@ printf("\n");
 else
 printf("Buzz ");
 }
-else if( a % 3 == 0)
+else
+if (a % 3 == 0)
 printf("Fizz ");
 else
 printf("%d ", a);
