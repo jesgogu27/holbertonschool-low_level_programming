5748cd7ed873ec27ac7503343300304462b487e4
jesgogu27 <jesgogu@hotmail.com>
2020-02-18 12:11:37 -0500

    count length

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,10 +2,12 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+#include <string.h>
 
 
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /* HOLBERTON_H */
