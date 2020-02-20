2b0b4fbed6cef88e5c3b28851d05a12edb51ae22
jesgogu27 <jesgogu@hotmail.com>
2020-02-20 18:33:59 -0500

    array inverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *reverse_array - print array inverse
+ *@a: array received
+ *@n: size of array
+ *Return: inverse array
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, v, j;
+int aux[n];
+
+j = 0;
+
+for (i = n ; i > 0; i--)
+{
+v = a[i - 1];
+aux[j] = v;
+j++;
+}
+
+for (j = 0 ; j < n ; j++)
+{
+a[j] = aux[j];
+}
+}
