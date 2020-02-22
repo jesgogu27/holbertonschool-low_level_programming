8b03b9906e106921fc230d13079c51151b65234a
jesgogu27 <jesgogu@hotmail.com>
2020-02-21 20:12:28 -0500

    pinter number

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -4,38 +4,38 @@
  *@n: number received
  * Return: Return the last digit
  */
+
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		n = n * (-1);
-		_putchar ('-');
-	}
-	while (n < 10)
-	{
-		_putchar (n + '0');
-		break;
-	}
-	while (n > 9 && n < 100)
-	{
-		_putchar ((n / 10) + '0');
-		_putchar ((n % 10) + '0');
-		break;
-	}
-	while (n > 9 && n > 100 && n < 1000)
-	{
-		_putchar ((n / 100) + '0');
-		_putchar ((n % 100) / 10 + '0');
-		_putchar ((n % 100) % 10 + '0');
-		break;
-
-	}
-	while (n > 9 && n > 100 && n > 1000)
-	{
-		_putchar ((n / 1000) + '0');
-		_putchar ((n % 1000) / 100 + '0');
-		_putchar (((n % 1000) % 100) / 10 + '0');
-		_putchar (((n % 1000) % 100) % 10 + '0');
-		break;
-	}
+if (n < 0)
+{
+n = n * (-1);
+_putchar ('-');
+}
+while (n < 10)
+{
+_putchar (n + '0');
+break;
+}
+while (n > 9 && n < 100)
+{
+_putchar ((n / 10) + '0');
+_putchar ((n % 10) + '0');
+break;
+}
+while (n > 9 && n > 100 && n < 1000)
+{
+_putchar ((n / 100) + '0');
+_putchar ((n % 100) / 10 + '0');
+_putchar ((n % 100) % 10 + '0');
+break;
+}
+while (n > 9 && n > 100 && n > 1000)
+{
+_putchar ((n / 1000) + '0');
+_putchar ((n % 1000) / 100 + '0');
+_putchar (((n % 1000) % 100) / 10 + '0');
+_putchar (((n % 1000) % 100) % 10 + '0');
+break;
+}
 }
