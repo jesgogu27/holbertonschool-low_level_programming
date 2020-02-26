43f4dd3ea8c95ed4957e4b0b44393d5ad770267a
jesgogu27 <jesgogu@hotmail.com>
2020-02-26 17:19:06 -0500

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_pow_recursion - that returns the factorial of a given number
+ *@x: base number.
+ *@y: exponential number.
+ *Return: an integer.
+ */
+
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+return (x * (_pow_recursion(x, y - 1)));
+}
