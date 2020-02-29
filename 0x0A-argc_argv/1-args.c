eb6b41ef8d68ca6827386f0134aa59f8ae75bd66
jesgogu27 <jesgogu@hotmail.com>
2020-02-29 18:49:22 -0500

    counts arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,6 @@
  *Return: always 0.
  */
 
-
 int main(int argc, __attribute__((unused)) char *argv[])
 {
 int i, sum;
@@ -25,5 +24,5 @@ sum += 1;
 }
 printf("%d\n", sum);
 }
-return (1);
+return (0);
 }
