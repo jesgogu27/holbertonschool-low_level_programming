833debbab12ea5df965bf7956f2383aef2939588
jesgogu27 <jesgogu@hotmail.com>
2020-02-29 18:32:35 -0500

    count arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (int argc, __attribute__((unused)) char* argv[])
+/**
+ *main - received two parameters
+ *@argc: count.
+ *@argv: vector.
+ *Return: always 0.
+ */
+
+
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	
-	int i, sum;
-	
-	if(argc >1)
-	{
-		for(i=1; i < argc; i++)
-		{
-			sum += 1 ;
-		}
-		printf("%d\n", sum);
-		
-	}
-	return (0);
+int i, sum;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+sum += 1;
+}
+printf("%d\n", sum);
+}
+return (1);
 }
