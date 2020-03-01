d1a39838fbcbe10ff346e0637aa814e9150ccfe4
jesgogu27 <jesgogu@hotmail.com>
2020-02-29 19:20:43 -0500

    print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,18 +3,18 @@
 
 /**
  *main - received two parameters
- *@argc: count.
- *@argv: vector.
+ *@argc: count
+ *@argv: vector
  *Return: value
  */
 int main(int argc, char *argv[])
 {
-int i, sum;
-sum = 0;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-sum += 1;
-}
-return (1);
+	int i, sum;
+	sum = 0;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		sum += 1;
+	}
+	return (1);
 }
