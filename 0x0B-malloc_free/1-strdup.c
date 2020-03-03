2259234ed5f2b748890c5014161629d33a7f41c4
jesgogu27 <jesgogu@hotmail.com>
2020-03-03 14:56:24 -0500

    string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *_strdup - print string
+ *srt: string.
+ *Return: string
+ */
+
+
+char *_strdup(char *str)
+{
+char *p;
+int i, c;
+
+c = 1;
+
+if (str == NULL)
+	return (NULL);
+
+for (i = 0; str[i] != '\0'; i++)
+	c++;
+
+p = malloc(c);
+
+if (p == NULL)
+	return (NULL);
+
+for (i = 0; str[i] != '\0'; i++)
+	p[i] = str[i];
+
+return (p);
+
+}
