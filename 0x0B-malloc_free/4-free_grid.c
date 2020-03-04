15c66b1ec4bd230c444b8cc6a9d8c9ca22abcb39
jesgogu27 <jesgogu@hotmail.com>
2020-03-03 22:56:25 -0500

    free memory

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,4 +9,16 @@
 
 void free_grid(int **grid, int height)
 {
+{
+	int i;
+
+	if (grid == NULL || grid == 0)
+	{
+		return;
+	}
+	for (i = 0; i < height;  i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
