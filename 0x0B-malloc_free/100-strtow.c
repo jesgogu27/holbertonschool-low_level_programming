d89d47ff689a077f47414198efc25d06ce263b94
jesgogu27 <jesgogu@hotmail.com>
2020-03-04 23:04:14 -0500

    5 and 6

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,12 +1,30 @@
 #include "holberton.h"
 
 /**
- *strtow - that splits a string into words.
- *@str: pointer.
- *Return: a pointer toa n array of string, words.
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
  */
-
-
 char **strtow(char *str)
 {
+	int i, x;
+	char *a;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+
+	a = malloc(i * sizeof(char) + 1);
+
+	if (a == 0)
+		return (NULL);
+
+	for (x = 0; x < i; x++)
+		a[x] = str[x];
+
+	return (*a);
 }
--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,12 +1,45 @@
 #include "holberton.h"
 
 /**
- *argstostr - concatenates all the arguments.
- *@ac: number of arguments.
- *@av: pointer argv.
- *Returns: Each arguments.
+ *argstostr - concatenates all the arguements of your program
+ *@ac: arguement count
+ *@av: argument values
+ * Return: pointer to string
  */
 
-char *argstostr(int ac, char **av) 
+char *argstostr(int ac, char **av)
 {
+	char *str, *begin;
+	int i, j, count;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			j++;
+			count++;
+		}
+		count++;
+	}
+	count++;
+	str = malloc(count * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	begin = str;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			*str = av[i][j];
+			j++;
+			str++;
+		}
+		*str = '\n';
+		str++;
+	}
+	return (begin);
 }
