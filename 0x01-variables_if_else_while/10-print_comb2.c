a818f06949de8f099f1ddebbe0539881feb5c280
jesgogu27 <jesgogu@hotmail.com>
2020-03-06 08:35:52 -0500

    verification

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,34 +9,34 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main (void)
 {
-int a;
-int b;
-a = 48;
-b = 48;
-while (a < 58)
-{
-while (b < 58)
-{
-putchar(a);
-if (b != 57)
-{
-putchar(b);
-putchar(',');
-putchar(32);
+	int a;
+	int b;
+	a = 48;
+	b = 48;
+	while (a < 58)
+	{
+		while (b < 58)
+		{
+			putchar(a);
+			if (b != 57)
+			{
+				putchar(b);
+				putchar(',');
+				putchar(32);
+			}
+			else
+			{
+				putchar(b);
+				putchar(32);
+			}
+			b++;
+		}
+		b = 48;
+		a++;
+	}
+	putchar('\n');
+	return (0);
 }
-else
-{
-putchar(b);
-putchar(32);
-}
-b++;
-}
-b = 48;
-a++;
-}
-putchar('\n');
-return (0);
-}
-
