0de83ba89b896b2a53b8eb7f99651fc6daf12642
jesgogu27 <jesgogu@hotmail.com>
2020-03-09 14:38:01 -0500

    head & macros

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _3_function_like_macro_h_
+#define _3_function_like_macro_h_
+#define ABS(x) ((x < 0) ? ((x) * -1) : x)
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef _4_sum_h_
+#define _4_sum_h_
+#define SUM(a, b) (a + b)
+
+#endif
