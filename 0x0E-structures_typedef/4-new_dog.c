b401d94be119aa56d2cd470354a73d2380b12b22
jesgogu27 <jesgogu@hotmail.com>
2020-03-10 14:26:26 -0500

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,10 +17,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
+return (new_dog);
+free(new_dog);
 }
 
 
