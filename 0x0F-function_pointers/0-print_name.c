c8a8a3a3b0f0c371f170e726c1ff028a64905cb9
jesgogu27 <jesgogu@hotmail.com>
2020-03-12 08:53:31 -0500

    mod 00

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,9 +10,8 @@ void print_name(char *name, void (*f)(char *))
 {
 
 
-if ((name == NULL) && (f == NULL))
+if (f != NULL)
 {
-
-}
 f(name);
+}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define _function_pointers_h_
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
