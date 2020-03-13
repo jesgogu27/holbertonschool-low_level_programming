be99b2935c42b244bfcc9b757f5fcaef990644cc
jesgogu27 <jesgogu@hotmail.com>
2020-03-13 16:57:26 -0500

    print numbers separate by character

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers
+ * @separator: character separator
+ * @n: number the arguments
+ * Return: Always 0.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+if (separator == NULL)
+{
+return;
+}
+
+unsigned int i;
+int a = 0;
+
+
+va_list args;
+va_start(args, n);
+
+
+for (i = 0; i < n; i++)
+{
+a = va_arg(args, int);
+printf("%i", a);
+if (n != i + 1)
+printf("%s", separator);
+}
+printf("\n");
+va_end(args);
+}
