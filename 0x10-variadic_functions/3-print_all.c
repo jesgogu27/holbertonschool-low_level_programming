38a210b32a9e4493585b7d6ec155dad00b4edb90
jesgogu27 <jesgogu@hotmail.com>
2020-03-15 09:54:10 -0500

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,7 +9,7 @@
  */
 void p_carac(va_list args)
 {
-printf("%c", va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
 
 /**
@@ -19,7 +19,7 @@ printf("%c", va_arg(args, int));
  */
 void p_ent(va_list args)
 {
-printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
 
 /**
@@ -29,7 +29,7 @@ printf("%d", va_arg(args, int));
  */
 void p_flot(va_list args)
 {
-printf("%f", va_arg(args, double));
+	printf("%f", va_arg(args, double));
 }
 
 /**
@@ -40,15 +40,15 @@ printf("%f", va_arg(args, double));
 void p_cad(va_list args)
 {
 
-char *j;
-j = va_arg(args, char*);
+	char *j;
+	j = va_arg(args, char*);
 
-if (j == NULL)
-{
-printf("(nill)");
-return;
-}
-printf("%s", j);
+	if (j == NULL)
+	{
+		printf("(nill)");
+		return;
+	}
+	printf("%s", j);
 }
 
 /**
@@ -60,41 +60,40 @@ printf("%s", j);
 void print_all(const char * const format, ...)
 {
 
-int i, j;
-char *separator = "";
-va_list args;
+	int i, j;
+	char *separator = "";
+	va_list args;
 
-cf ptr[] = {
-{"c", p_carac},
-{"i", p_ent},
-{"f", p_flot},
-{"s", p_cad}
-};
+	cf ptr[] = {
+		{"c", p_carac},
+		{"i", p_ent},
+		{"f", p_flot},
+		{"s", p_cad}
+	};
 
-va_start(args, format);
+	va_start(args, format);
 
-i = 0;
-j = 0;
+	i = 0;
+	j = 0;
 
+	while (format != NULL && *(format + i) != '\0')
+	{
 
-while (format != NULL && *(format + i) != '\0')
-{
+		while (j < 4)
+		{
+			if (ptr[j].cacrec[0] == format[i])
+			{
+				printf("%s", separator);
+				ptr[j].f(args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
 
-while (j < 4)
-{
-if (ptr[j].cacrec[0] == format[i])
-{
-printf("%s", separator);
-ptr[j].f(args);
-separator = ", ";
-break;
-}
-j++;
-}
-i++;
-j = 0;
-}
-
-va_end(args);
-printf("\n");
+	va_end(args);
+	printf("\n");
 }
