560223924d7640d34f340aba1e432c0f8a88f802
jesgogu27 <jesgogu@hotmail.com>
2020-03-19 23:18:49 -0500

    style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <string.h>
 /**
- * add_node - add a new node 
+ * add_node - add a new node
  * @head: double pointer to head
  * @str: string to be saved in new node, must be duplicated
  * Return: Address of new element or NULL if failed
@@ -16,15 +16,15 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		count++;
 	}
-
 	if (str == NULL)
+	{
 		return (0);
-
+	}
 	new = malloc(sizeof(list_t));
-
 	if (new == NULL)
+	{
 		return (0);
-
+	}
 	new->str = strdup(str);
 	new->len = count;
 	new->next = *head;
