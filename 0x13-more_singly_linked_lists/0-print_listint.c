e50b11aef9ea89656ea4a4132efbd351ff0ca814
jesgogu27 <jesgogu@hotmail.com>
2020-03-24 01:55:10 -0500

    print all the elemnts of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_listint - prints all the elements of a listint_t list.
+ *@h: struct that content data.
+ *Return: the number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+int i = 0;
+
+if (h != NULL)
+{
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
+else
+{
+printf("NULL \n");
+return (0);
+}
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+**README**
+
+
+
+  
+    How to use linked lists
+    Start to look for the right source of information without too much help
+
