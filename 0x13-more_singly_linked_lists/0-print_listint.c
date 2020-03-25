732ee9f0966402a28ac43d649e91d8a290d69f32
jesgogu27 <jesgogu@hotmail.com>
2020-03-24 21:28:45 -0500

    exercise

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
+int i = 0;
 
-	int i = 0;
-
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-	return (i);
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
 }
