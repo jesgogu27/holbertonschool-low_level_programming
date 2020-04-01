ebc6889caa655f22577d5dce591e47cdc1a253c8
jesgogu27 <jesgogu@hotmail.com>
2020-04-01 01:33:04 -0500

    create file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,40 @@
 #include "holberton.h"
 
 /**
-*read_textfile - read file.
-*@filename: name of file
-*@letters: size of file.
-*Return: number of byte
-*/
+ *read_textfile - read file.
+ *@filename: name of file
+ *@letters: size of file.
+ *Return: number of byte
+ */
 
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t nr_bytes;
-char *buf;
-int fd, r;
+	ssize_t nr_bytes;
+	char *buf;
+	int fd, r;
 
-buf = malloc(sizeof(char) * letters);
-fd = open(filename, O_RDONLY);
+	buf = malloc(sizeof(char) * letters);
+	fd = open(filename, O_RDONLY);
 
-if (fd == -1)
-{
-return (0);
-}
-else
-{
-nr_bytes = read(fd, buf, letters);
-if (nr_bytes == -1)
-{
-return (0);
-}
-else
-{
-r = write(STDOUT_FILENO, buf, nr_bytes);
-if (r == -1)
-return (0);
-}
-}
-close(fd);
-return (r);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	else
+	{
+		nr_bytes = read(fd, buf, letters);
+		if (nr_bytes == -1)
+		{
+			return (0);
+		}
+		else
+		{
+			r = write(STDOUT_FILENO, buf, nr_bytes);
+			if (r == -1)
+				return (0);
+		}
+	}
+	close(fd);
+	return (r);
 }
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+*create_file - create file.
+*@filename: name of file
+*@text_content: content file.
+*Return: number of byte
+*/
+
+
+int create_file(const char *filename, char *text_content)
+{
+
+int fd, i, r;
+
+mode_t tmode = O_CREAT | O_WRONLY | O_TRUNC;
+mode_t pmode = S_IRUSR | S_IWUSR;
+
+fd = open(filename, tmode, pmode);
+
+if (fd == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+for (i = 0; text_content[i] != '\0'; i++)
+;
+
+r = write(fd, text_content, i);
+
+if (r == -1)
+return (-1);
+}
+closed(fd);
+return (1);
+}
+
+
