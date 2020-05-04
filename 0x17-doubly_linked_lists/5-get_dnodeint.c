9de9b894b54977e2e171d8e6a522f71ed4a057c7
jesgogu27 <jesgogu@hotmail.com>
2020-05-03 23:49:01 -0500

    index and add

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - get the nth node of a `dlistint_t` doubly LL
- * @head: pointer to head of LL
- * @index: node index to return
- * Return: node at index given or NULL if node does not exist
+ * get_dnodeint_at_index - get the nth node
+ * @head: pointer to head Dll
+ * @index: node index
+ * Return: node at index given
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	while (head != NULL && count< index)
+	while (head != NULL && count < index)
 	{
 		head = head->next;
 		count++;
