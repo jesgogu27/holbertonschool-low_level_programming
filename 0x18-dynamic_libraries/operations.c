91600d9b88e789f67a9578708062cc40ec85426f
jesgogu27 <jesgogu@hotmail.com>
2020-05-04 22:06:51 -0500

    operations in python with C

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+*add - add 2 numbers
+*@a: number a
+*@b: number b
+*Return: result of add
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*sub - sub 2 numbers
+*@a: number a
+*@b: number b
+*Return: result of sub
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*mul - mul 2 numbers
+*@a: number a
+*@b: number b
+*Return: result of mul
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*div - div 2 numbers
+*@a: number a
+*@b: number b
+*Return: result of div
+*/
+
+int div(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*mod - mod 2 numbers
+*@a: number a
+*@b: number b
+*Return: result of mod
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
+
