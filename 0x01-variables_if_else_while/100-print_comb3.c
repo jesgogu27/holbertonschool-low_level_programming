fe06c7da530cef3c99faec20f85252d008ac6226
jesgogu27 <jesgogu@hotmail.com>
2020-05-27 22:16:25 -0500

    Combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,47 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int a;
+	int b;
+
+	a = 48;
+	b = 48;
+
+	while (a < 58)
+	{
+
+		while (b < 58)
+		{
+
+			if ((b != a) && (a < b))
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(32);
+			}
+
+			if ((b == 57) && (a == 57))
+			{
+				putchar('\n');
+			}
+
+			b = b + 1;
+		}
+
+		a = a + 1;
+		b = 48;
+
+	}
+
+	return (0);
+}
