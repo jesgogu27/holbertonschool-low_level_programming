782dd663607fc19ffbc951294a05d20eb745ed3c
jesgogu27 <jesgogu@hotmail.com>
2020-05-29 18:22:44 -0500

    index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - return a index of a key
+ * @key: key to get index
+ * @size: size of array of hash table
+ * Return: index of the key
+ */
+
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
