51bcbe1fe7bf6da4662518e3be9fb31f491e9144
jesgogu27 <jesgogu@hotmail.com>
2020-05-29 17:07:33 -0500

    creating hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+*hash_table_create - Create a hash table
+*@size: size of array 
+*Return: pointer to new hash table. 
+*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -13,10 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	ht->size = size;
 
-	ht->array = calloc(size, sizeof(hash_table_t));
+	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
-		free(ht);
 		return (NULL);
 	}
 	return (ht);
