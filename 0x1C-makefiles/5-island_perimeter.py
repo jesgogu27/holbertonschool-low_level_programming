7ef36b6110f8caaa756a406ac6d950a421efd4ff
jesgogu27 <jesgogu@hotmail.com>
2020-07-05 12:02:44 -0500

    task 5 and 6

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island
+
+    """
+    p = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                p += 4
+                if x > 0 and grid[x - 1][y] == 1:
+                    p -= 2
+                if y > 0 and grid[x][y - 1] == 1:
+                    p -= 2
+    return p
